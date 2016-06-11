f9bb3bea392bf36999a7a5c93aa3282259eed199
Jackson <jackson@borneman.com>
2016-06-11 09:34:34 -0400

    a

--- DND.java
+++ DND.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class DND{
    public static void main(String[] args){
-     Scanner keyboard = new Scanner(System.in); 
+     Scanner keyboard = new Scanner(System.in);
+     String Name = keyboard.next();
+     System.out.println(""+Name);
    }
 }
