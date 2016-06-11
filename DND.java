9b7a08ae672614cac9720cc1eb09e729cb131cde
Jackson <jackson@borneman.com>
2016-06-11 09:40:15 -0400

    added partial outputs

--- DND.java
+++ DND.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 public class DND{
    public static void main(String[] args){
-     Scanner keyboard = new Scanner(System.in);
-     String Name = keyboard.next();
-     System.out.println(""+Name);
+      Scanner keyboard = new Scanner(System.in);
+      String Name = keyboard.next();
+     
+     
+     
+     
+      System.out.println("Name:"+Name);
+      System.out.println("  Strength:"+"");
+      System.out.println("  Constitution:"+"");
+      System.out.println("  Dexterity:"+"");
+      System.out.println("  Intelligence:"+"");
+      System.out.println("  Wisdom:"+"");
+      System.out.println("  Charisma:"+"");
    }
 }
